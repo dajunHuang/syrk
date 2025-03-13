2c73efb0df982b6cbc4a900c03c398b53c8f0cc0
djhuang <djhuang_1@qq.com>
2025-03-13 21:29:06 +0800

    modify test script

--- test.sh
+++ test.sh
@@ -3,15 +3,12 @@
 programs=("syr2k_double" "syr2k_float" "syrk_double" "syrk_float" "trmm_double" "trmm_float" "trsm_double" "trsm_float")
 # programs=("syrk_double")
 
-ms0=(1024 2048 4096 8192 16384)
-ns0=(1024 2048 4096 8192 16384)
-
-ms1=(16384 16384 16384 16384)
-ns1=(1024 2048 4096 8192)
-
-ms2=(1024 2048 4096 8192)
-ns2=(16384 16384 16384 16384)
-
+ms0=(8192 16384 24576 40960 49152 57344)
+ns0=(8192 16384 24576 40960 49152 57344)
+ms1=(57344 57344 57344 57344 57344)
+ns1=(8192 16384 24576 40960 49152)
+ms2=(8192 16384 24576 40960 49152)
+ns2=(57344 57344 57344 57344 57344)
 
 nb_values=(128 256 512 1024 2048 4096)
 
@@ -28,102 +25,35 @@ for prog in "${programs[@]}"; do
     best_custom_TFLOPS=0
     best_custom_latency=0
     best_nb=0
-    best_cublas_latency=0
-    best_cublas_TFLOPS=0
+    cublas_latency=0
+    cublas_TFLOPS=0
+
+    cublas_output=$(./${prog}_cublas $m $n)
+    cublas_line=$(echo "$cublas_output" | grep "cublas")
+    cublas_latency=$(echo "$cublas_line" | awk -F'[, ]+' '{print $8}')
+    cublas_TFLOPS=$(echo "$cublas_line" | awk -F'[, ]+' '{print $10}')
 
     for nb in "${nb_values[@]}"; do
       if [ $nb -le $m ]; then
         echo "processing: ./$prog $m $n $nb 0"
 
-        output=$(./$prog $m $n $nb 0)
-
+        output=$(./${prog} $m $n $nb 0)
         custom_line=$(echo "$output" | grep "custom")
-        cublas_line=$(echo "$output" | grep "cublas")
         custom_latency=$(echo "$custom_line" | awk -F'[, ]+' '{print $8}')
         custom_TFLOPS=$(echo "$custom_line" | awk -F'[, ]+' '{print $10}')
-        cublas_latency=$(echo "$cublas_line" | awk -F'[, ]+' '{print $8}')
-        cublas_TFLOPS=$(echo "$cublas_line" | awk -F'[, ]+' '{print $10}')
         
         if awk 'BEGIN { if ('"$custom_TFLOPS"' > '"$best_custom_TFLOPS"') exit 0; else exit 1 }'; then
           best_custom_TFLOPS=$custom_TFLOPS
           best_custom_latency=$custom_latency
           best_nb=$nb
-          best_cublas_latency=$cublas_latency
-          best_cublas_TFLOPS=$cublas_TFLOPS
         fi
       fi
     done
 
-    echo "$m,$n,$best_custom_latency,$best_custom_TFLOPS,$best_nb,$best_cublas_latency,$best_cublas_TFLOPS" >> "$output_file"
+    echo "$m,$n,$best_custom_latency,$best_custom_TFLOPS,$best_nb,$cublas_latency,$cublas_TFLOPS" >> "$output_file"
   done
   # mn1
-  for index in "${!ms1[@]}"; do
-    m=${ms1[$index]}
-    n=${ns1[$index]}
-    best_custom_TFLOPS=0
-    best_custom_latency=0
-    best_nb=0
-    best_cublas_latency=0
-    best_cublas_TFLOPS=0
 
-    for nb in "${nb_values[@]}"; do
-
-        echo "processing: ./$prog $m $n $nb 0"
-
-        output=$(./$prog $m $n $nb 0)
-
-        custom_line=$(echo "$output" | grep "custom")
-        cublas_line=$(echo "$output" | grep "cublas")
-        custom_latency=$(echo "$custom_line" | awk -F'[, ]+' '{print $8}')
-        custom_TFLOPS=$(echo "$custom_line" | awk -F'[, ]+' '{print $10}')
-        cublas_latency=$(echo "$cublas_line" | awk -F'[, ]+' '{print $8}')
-        cublas_TFLOPS=$(echo "$cublas_line" | awk -F'[, ]+' '{print $10}')
-
-        if awk 'BEGIN { if ('"$custom_TFLOPS"' > '"$best_custom_TFLOPS"') exit 0; else exit 1 }'; then
-          best_custom_TFLOPS=$custom_TFLOPS
-          best_custom_latency=$custom_latency
-          best_nb=$nb
-          best_cublas_latency=$cublas_latency
-          best_cublas_TFLOPS=$cublas_TFLOPS
-        fi
-    
-    done
-
-    echo "$m,$n,$best_custom_latency,$best_custom_TFLOPS,$best_nb,$best_cublas_latency,$best_cublas_TFLOPS" >> "$output_file"
-  done
   # mn2
-  for index in "${!ms2[@]}"; do
-    m=${ms2[$index]}
-    n=${ns2[$index]}
-    best_custom_TFLOPS=0
-    best_custom_latency=0
-    best_nb=0
-    best_cublas_latency=0
-    best_cublas_TFLOPS=0
 
-    for nb in "${nb_values[@]}"; do
-      if [ $nb -le $m ]; then
-        echo "processing: ./$prog $m $n $nb 0"
-
-        output=$(./$prog $m $n $nb 0)
-
-        custom_line=$(echo "$output" | grep "custom")
-        cublas_line=$(echo "$output" | grep "cublas")
-        custom_latency=$(echo "$custom_line" | awk -F'[, ]+' '{print $8}')
-        custom_TFLOPS=$(echo "$custom_line" | awk -F'[, ]+' '{print $10}')
-        cublas_latency=$(echo "$cublas_line" | awk -F'[, ]+' '{print $8}')
-        cublas_TFLOPS=$(echo "$cublas_line" | awk -F'[, ]+' '{print $10}')
-
-        if awk 'BEGIN { if ('"$custom_TFLOPS"' > '"$best_custom_TFLOPS"') exit 0; else exit 1 }'; then
-          best_custom_TFLOPS=$custom_TFLOPS
-          best_custom_latency=$custom_latency
-          best_nb=$nb
-          best_cublas_latency=$cublas_latency
-          best_cublas_TFLOPS=$cublas_TFLOPS
-        fi
-      fi
-    done
-
-    echo "$m,$n,$best_custom_latency,$best_custom_TFLOPS,$best_nb,$best_cublas_latency,$best_cublas_TFLOPS" >> "$output_file"
-  done
 done
